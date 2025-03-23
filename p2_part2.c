f54e77f8d3747d4115f248adc956b6c1c85e4b8c
Ram Mude <ramdmude3061@gmail.com>
2025-03-23 17:31:02 -0400

    Added p2 part 2-3

--- p2_part2.c
+++ p2_part2.c
@@ -9,6 +9,8 @@
 #include <linux/types.h>
 #include <linux/kprobes.h>
 #include <linux/stacktrace.h>
+#include <linux/rbtree.h>
+#include <linux/jhash.h>
 
 #include "p2_part2.h"
 
@@ -25,9 +27,11 @@ extern unsigned int stack_trace_save_user(unsigned long *store, unsigned int siz
  */
 #define MY_HASH_TABLE_BINS 10
 #define MAX_STACK_TRACE 16
+#define MAX_DEPTH 4
+#define MAX_TOP_TASKS 20
 static DEFINE_HASHTABLE(sched_htable, MY_HASH_TABLE_BINS);
 
-/* Hashtable entry struct */
+
 #if (P2_PART2 == 1)
 struct sched_hentry {
     unsigned long stack_entries[MAX_STACK_TRACE];
@@ -41,12 +45,20 @@ struct sched_hentry {
     unsigned long stack_entries[MAX_STACK_TRACE];
     unsigned int nr_entries;
     u64 total_exec_time;
-    u64 actual_exec;
     struct hlist_node hash;
 };
 
+#elif (P2_PART2 == 3)
+struct sched_rbentry {
+    struct rb_node node;
+    u64 exec_time;
+    u32 stack_hash;
+    unsigned long stack_entries[MAX_STACK_TRACE];
+    unsigned int nr_entries;
+};
 #endif
-/* HASH TABLE END */
+
+static struct rb_root sched_rbtree = RB_ROOT;
 
 static char symbol2[KSYM_NAME_LEN] = "pick_next_task_fair";
 module_param_string(symbol2, symbol2, KSYM_NAME_LEN, 0644);
@@ -140,7 +152,6 @@ static void __kprobes handler_post2(struct kprobe *p, struct pt_regs *regs, unsi
             if (entry->nr_entries == nr_entries &&
                 !memcmp(entry->stack_entries, entries, nr_entries * sizeof(unsigned long))) {
                 entry->total_exec_time += elapsed;
-                entry->actual_exec = curr->se.sum_exec_runtime;
                 goto update_prev;
             }
         }
@@ -152,7 +163,6 @@ static void __kprobes handler_post2(struct kprobe *p, struct pt_regs *regs, unsi
         memcpy(entry->stack_entries, entries, nr_entries * sizeof(unsigned long));
         entry->nr_entries = nr_entries;
         entry->total_exec_time = elapsed;
-        entry->actual_exec = curr->se.sum_exec_runtime;
         hash_add(sched_htable, &entry->hash, hash_key);
     }
 update_prev:
@@ -173,13 +183,108 @@ static int lkp25_p2_proc_show(struct seq_file *m, void *v)
             seq_printf(m, "%pS\n", (void *)entry->stack_entries[i]);
         }
         seq_printf(m, "Total Time: %llu ns\n\n", entry->total_exec_time);
-        seq_printf(m, "Actual Time: %llu ns\n\n", entry->actual_exec);
     }
     return 0;
 }
 
+#elif (P2_PART2 == 3)
+
+static struct task_struct *prev_task = NULL;
+static u64 prev_timestamp = 0;
+
+static struct sched_rbentry *rbtree_lookup(u64 exec_time) {
+    struct rb_node *node = sched_rbtree.rb_node;
+    while (node) {
+        struct sched_rbentry *entry = container_of(node, struct sched_rbentry, node);
+        if (exec_time < entry->exec_time)
+            node = node->rb_left;
+        else if (exec_time > entry->exec_time)
+            node = node->rb_right;
+        else
+            return entry;
+    }
+    return NULL;
+}
+
+static void rbtree_insert(struct sched_rbentry *new_entry) {
+    struct rb_node **link = &sched_rbtree.rb_node, *parent = NULL;
+    while (*link) {
+        struct sched_rbentry *entry = container_of(*link, struct sched_rbentry, node);
+        parent = *link;
+        if (new_entry->exec_time < entry->exec_time)
+            link = &(*link)->rb_left;
+        else
+            link = &(*link)->rb_right;
+    }
+    rb_link_node(&new_entry->node, parent, link);
+    rb_insert_color(&new_entry->node, &sched_rbtree);
+}
+
+static void rbtree_remove(struct sched_rbentry *entry) {
+    rb_erase(&entry->node, &sched_rbtree);
+    kfree(entry);
+}
+
+static void __kprobes handler_post2(struct kprobe *p, struct pt_regs *regs, unsigned long flags)
+{
+    struct task_struct *curr = (struct task_struct *)regs->si;
+    if (!curr) return;
+
+    unsigned long entries[MAX_STACK_TRACE];
+	unsigned int nr_entries;
+
+    u64 now = ktime_get_ns();
+    if (prev_task && prev_timestamp){
+        u64 elapsed = now - prev_timestamp;
+    
+        if (user_mode(regs))
+            nr_entries = stack_trace_save_user(entries, MAX_STACK_TRACE);
+        else
+            nr_entries = stack_trace_save(entries, MAX_STACK_TRACE, 0 );
+
+        u32 stack_hash = jhash(entries, nr_entries * sizeof(unsigned long), 0);
+
+        struct sched_rbentry *old_entry = rbtree_lookup(prev_task->se.sum_exec_runtime);
+        if (old_entry)
+            rbtree_remove(old_entry);
+        
+        struct sched_rbentry *new_entry = kmalloc(sizeof(*new_entry), GFP_KERNEL);
+        if (!new_entry)
+            return;
+        
+        new_entry->exec_time = prev_task->se.sum_exec_runtime + elapsed;
+        new_entry->stack_hash = stack_hash;
+        memcpy(new_entry->stack_entries, entries, nr_entries * sizeof(unsigned long));
+        new_entry->nr_entries = nr_entries;
+        
+        rbtree_insert(new_entry);
+    }
+    prev_task = curr;
+    prev_timestamp = now;
+	return;
+}
+
+static int lkp25_p2_proc_show(struct seq_file *m, void *v)
+{
+	struct rb_node *node;
+    int count = 0;
+    
+    seq_puts(m, "Rank\tJenkins Hash\tTotal CPU Time (ns)\tStack Trace\n");
+    for (node = rb_last(&sched_rbtree); node && count < MAX_TOP_TASKS; node = rb_prev(node), count++) {
+        struct sched_rbentry *entry = container_of(node, struct sched_rbentry, node);
+        seq_printf(m, "%d\t%u\t%llu\t", count + 1, entry->stack_hash, entry->exec_time);
+        for (int i = 0; i < min(entry->nr_entries, MAX_DEPTH); i++)
+            seq_printf(m, "%pS ", (void *)entry->stack_entries[i]);
+        seq_puts(m, "\n");
+    }
+
+    return 0;
+}
+
+
 #endif
 
+#if (P2_PART2 == 1 || P2_PART2 == 2)
 static void cleanup_sched_htable(void) {
     struct sched_hentry *entry;
     struct hlist_node *tmp;
@@ -191,6 +296,17 @@ static void cleanup_sched_htable(void) {
     }
 
 }
+#elif (P2_PART2 == 3)
+static void cleanup_rbtree(void) {
+    struct rb_node *node, *next;
+    for (node = rb_first(&sched_rbtree); node; node = next) {
+        struct sched_rbentry *entry = container_of(node, struct sched_rbentry, node);
+        next = rb_next(node);
+        rb_erase(node, &sched_rbtree);
+        kfree(entry);
+    }
+}
+#endif
 
 static int lkp25_p2_proc_open(struct inode *inode, struct file *file)
 {
@@ -231,7 +347,12 @@ static void __exit lkp25_p2_exit(void)
 	/* Remove the /proc/perftop entry */
 	unregister_kprobe(&kp2);
 	printk(KERN_INFO "Scheduler kprobe at %p unregistered\n", kp2.addr);
-	cleanup_sched_htable();
+	
+    #if (P2_PART2 == 1 || P2_PART2 == 2)
+    cleanup_sched_htable();
+    #elif (P2_PART2 == 3)
+    cleanup_rbtree();
+    #endif
 	remove_proc_entry("perftop", NULL);
 	printk(KERN_INFO "lkp_p2 Module Unloaded");
 }
--- p2_part2.h
+++ p2_part2.h
@@ -4,6 +4,6 @@
 // 1 for part 2-1
 // 2 for part 2-2
 // 3 for part 2-3
-#define P2_PART2 2
+#define P2_PART2 3
 
 #endif
